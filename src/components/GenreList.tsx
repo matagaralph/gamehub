bae49b0d3261c8334b9c271b6367106c4a2b3dd1
Believe Mataga <believe@merza.co.zw>
2023-12-26 23:04:23 +0200

    Show a spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,27 @@
 import useGenres from '../hooks/useGenres';
+import getCroppedImage from '../services/image-url';
+import { Text } from './ui/text';
+import { FiLoader } from 'react-icons/fi';
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading } = useGenres();
+
+  if (isLoading)
+    return (
+      <FiLoader className="animate-spin size-8 text-default text-center mt-6" />
+    );
   return (
-    <ul className="mt-6">
+    <ul className="mt-6 list-none">
       {data.map((g) => (
-        <li className="text-default" key={g.id}>
-          {g.name}
+        <li className="py-2" key={g.id}>
+          <div className="flex gap-2.5">
+            <img
+              src={getCroppedImage(g.image_background)}
+              alt={g.name}
+              className="size-9 rounded-md"
+            />
+            <Text className="text-lg">{g.name}</Text>
+          </div>
         </li>
       ))}
     </ul>
