195fe807e7c8c730c290e2f5e4dcd509e7435a86
Believe Mataga <believe@merza.co.zw>
2023-12-26 17:28:50 +0200

    Add dependencies and styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './styles/index.css';
+import { ThemeProvider } from './components/ThemeProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider defaultTheme="light">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  safelist: ['icon-md', 'text-icon-default'],
+  darkMode: ['class', '[class*="dark-theme"]'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    borderRadius: {
+      none: 0,
+      xs: 2,
+      sm: 4,
+      md: 6,
+      lg: 10,
+      xl: 16,
+      '2xl': 20,
+      '3xl': 24,
+      full: 9999,
+    },
+    boxShadow: {
+      none: 'hsl(var(--merza-theme-shadows-none))',
+      xs: 'hsl(var(--merza-theme-shadows-xs))',
+      sm: 'hsl(var(--merza-theme-shadows-sm))',
+      md: 'hsl(var(--merza-theme-shadows-md))',
+      lg: 'hsl(var(--merza-theme-shadows-lg))',
+      xl: 'hsl(var(--merza-theme-shadows-xl))',
+    },
+    extend: {
+      backgroundColor: {
+        default: 'hsl(var(--merza-theme-background-default))',
+        screen: 'hsl(var(--merza-theme-background-screen))',
+        subtle: 'hsl(var(--merza-theme-background-subtle))',
+        element: 'hsl(var(--merza-theme-background-element))',
+        hover: 'hsl(var(--merza-theme-background-hover))',
+      },
+      colors: {
+        default: 'hsl(var(--merza-theme-text-default))',
+        secondary: 'hsl(var(--merza-theme-text-secondary))',
+      },
+      borderColor: {
+        default: 'hsl(var(--merza-theme-border-default))',
+        secondary: 'hsl(var(--merza-theme-border-secondary))',
+      },
+    },
   },
   plugins: [],
-}
-
+};
