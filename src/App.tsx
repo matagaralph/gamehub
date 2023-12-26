52cd2717303d809167c3cf5828dff0b5b6b80e6f
Believe Mataga <believe@merza.co.zw>
2023-12-26 18:11:22 +0200

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header className="h-14 border-b"></header>
+      <div className="flex flex-row flex-1">
+        <aside className="p-4 w-[280px] h-dvh relative bg-red-500 lg:flex hidden">
+          SideBar
+        </aside>
+        <main className="px-4 sm:px-6 lg:px-8 pt-8 bg-slate-500 flex-1">
+          Main
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
