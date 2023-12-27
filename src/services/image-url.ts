98e5d410273ef39517a5ff433328ed792ae9167b
Believe Mataga <believe@merza.co.zw>
2023-12-27 11:46:19 +0200

    Handle games with no images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImagePlaceholder from '../assets/no-image-placeholder.webp';
+
 function getCroppedImage(url: string) {
-  if (!url) return '';
+  if (!url) return noImagePlaceholder;
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
